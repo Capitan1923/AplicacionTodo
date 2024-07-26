9a774ce1b87c480b87a539cb811f09f551c2bf26
Bruno Palomino <73258272+Capitan1923@users.noreply.github.com>
2024-07-26 16:18:47 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import { TodoCounter } from './TodoCounter';
+import { TodoSearch } from './TodoSearch';
+import { TodoList } from './TodoList';
+import { TodoItem } from './TodoItem';
+import { CreateTodoButton } from './CreateTodoButton';
+
+const defaultTodos = [
+  { text: 'Cortar cebolla', completed: true },
+  { text: 'Tomar el Curso de Intro a React.js', completed: false },
+  { text: 'Llorar con la Llorona', completed: false },
+  { text: 'LALALALALA', completed: false },
+  { text: 'Usar estados derivados', completed: true },
+
+];
+
+function App() {
+  const[todos, setTodos]=React.useState
+  (defaultTodos);
+  const [searchValue, setSearchValue]=React.
+  useState('');
+
+  const completedTodos = todos.filter(
+    todo => !!todo.completed
+  ).length;
+  const totalTodos = todos.length;
+
+  const searchedTodos = todos.filter(todo =>
+    todo.text.toLowerCase().includes(searchValue.toLowerCase())
+  );
+  console.log('Los usuarios buscan todos de ' + 
+  searchValue);
+
+  return (
+    <>
+      <TodoCounter 
+      completed={completedTodos} 
+      total={totalTodos} 
+      />
+      <TodoSearch 
+      searchValue= {searchValue}
+      setSearchValue={setSearchValue}
+      />
+
+      <TodoList>
+        {searchedTodos.map(todo => (
+          <TodoItem
+            key={todo.text}
+            text={todo.text}
+            completed={todo.completed}
+          />
+        ))}
+      </TodoList>
+      
+      <CreateTodoButton />
+    </>
+  );
+}
+
+export default App;
---
+++ src/TodoSearch.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './TodoSearch.css';
+
+function TodoSearch({ searchValue, setSearchValue }) {
+  const onSearchValueChange = (event) => {
+    setSearchValue(event.target.value);
+  };
+
+  return (
+    <input
+      placeholder="Cortar cebolla"  
+      className="TodoSearch"
+      value={searchValue}
+      onChange={onSearchValueChange}
+    />
+  );
+}
+
+export { TodoSearch };
